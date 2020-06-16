a5fb36341be8117e93ae0fcad3e347c5471e962e
7wf <7ywolf@gmail.com>
2020-06-16 16:16:35 -0300

    feat(fastify/types): type definitions for websocket decorators

--- src/types/fastify/index.d.ts
+++ src/types/fastify/index.d.ts
@@ -2,13 +2,17 @@ import { PrismaClient } from '@prisma/client'
 
 import { StatikkAMQP } from '../../plugins/amqp'
 
+import { Server as WebSocketServer } from 'socket.io'
+
 declare module 'fastify' {
     export interface FastifyInstance {
+        websocket: WebSocketServer
         database: PrismaClient
     }
 
     export interface FastifyRequest {
         amqp: StatikkAMQP
         database: PrismaClient
+        websocket: WebSocketServer
     }
 }
